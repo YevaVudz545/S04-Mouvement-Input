d0244d06cdc9246ef97fdc9503a0f5342dd7d87d
YevaVudz545 <yevavudz545@gmail.com>
2025-02-12 17:50:48 -0500

    Force physique Balle

--- Assets/Scripts/MouvementRigidbodyForces.cs
+++ Assets/Scripts/MouvementRigidbodyForces.cs
@@ -9,9 +9,11 @@ public class MouvementRigidbodyForces : MonoBehaviour
 
     void Start()
     {
+
         // Prends une réference au Rigidbody attaché a cet objet.
         _rb = GetComponent<Rigidbody>();
 
-        _rb.AddForce(forceInitiale);
+
+        _rb.AddForce(forceInitiale, ForceMode.VelocityChange);
     }
 }
